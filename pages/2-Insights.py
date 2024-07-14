1dfd8bb816a02b2c11ee5b76f291892ca5625947
fmascara <fernandotanesemascara@gmail.com>
2024-07-14 20:38:11 -0300

    Update and rename 4-Insights.py to 2-Insights.py

--- pages/2-Insights.py
+++ pages/2-Insights.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import numpy as np
 from utils.resources import custom_sidebar, config_pagina
 
 config_pagina()
