a88e5cc76e582399da060137e06fd4c5564b81c1
fmascara <fernandotanesemascara@gmail.com>
2024-07-17 07:51:11 -0300

    Update resources.py

--- utils/resources.py
+++ utils/resources.py
@@ -3,11 +3,11 @@ import streamlit as st
 def custom_sidebar():
     st.sidebar.subheader("Equipe")
     st.sidebar.markdown("Alexandre Gomes de Araujo")
-    st.sidebar.markdown("*RM 123456789*")
+    st.sidebar.markdown("*RM 352922*")
     st.sidebar.markdown("Fernando Tanese Mascara")
-    st.sidebar.markdown("*RM 123456789*")
+    st.sidebar.markdown("*RM 352656*")
     st.sidebar.markdown("Jessica Thesin Zulian")
-    st.sidebar.markdown("*RM 123456789*")
+    st.sidebar.markdown("*RM 353013*")
 
 def config_pagina():
     st.set_page_config(
